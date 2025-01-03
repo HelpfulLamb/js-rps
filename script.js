ee43886364d30244bd2cdf2b9189c701f1290cf5
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-01-03 12:05:14 -0600

    Refactored some functions to display both computer choices and player choices for game results

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let computerScore = 0;
 let humanScore = 0;
+let compPlay = '';
+let humanPlay = '';
+
 
 function getComputerChoice(){
     // 1 = rock, 2 = paper, 3 = scissors
     let computer_choice = Math.floor(Math.random() * 3) + 1;
+    if(computer_choice === 1){
+        compPlay = 'Rock';
+    } else if(computer_choice === 2){
+        compPlay = 'Paper';
+    } else if(computer_choice === 3){
+        compPlay = 'Scissors';
+    }
+
     if(computer_choice === 1 || computer_choice === 2 || computer_choice === 3){
         return computer_choice;
-    } else{
+    } else {
         return "Unknown";
     }
 }
@@ -14,10 +25,13 @@ function getComputerChoice(){
 function getHumanChoice(){
     let player_choice = prompt("Choose: [1] Rock, [2] Paper, [3] Scissors").toLowerCase();
     if(player_choice === "rock"){
+        humanPlay = 'Rock';
         return 1;
     } else if(player_choice === "paper"){
+        humanPlay = 'Paper';
         return 2;
     } else if(player_choice === "scissors"){
+        humanPlay = 'Scissors';
         return 3;
     } else{
         return "Invalid Choice";
@@ -27,14 +41,14 @@ function getHumanChoice(){
 function playRound(computerChoice, humanChoice){
     if(computerChoice === humanChoice){
         console.log("Draw");
-    } else if((humanChoice == 1 && computerChoice == 3) || (humanChoice == 2 && computerChoice == 1) || (humanChoice == 3 && computerChoice == 2)){
+    } else if((humanChoice === 1 && computerChoice === 3) || (humanChoice === 2 && computerChoice === 1) || (humanChoice === 3 && computerChoice === 2)){
         console.log('You win');
         humanScore++;
     } else{
         console.log('You lose');
         computerScore++;
     }
-    console.log("Computer: " + computerChoice + " ; Human: " + humanChoice);
+    console.log("Computer: " + compPlay + " ; Human: " + humanPlay);
     console.log("Scores: Computer = " + computerScore + " ; Human = " + humanScore);
 }
 
