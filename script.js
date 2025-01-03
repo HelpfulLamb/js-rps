794aa2b17ea56f722979ababeff302468b887e58
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-01-03 10:52:47 -0600

    Created two functions, one for winner and another for playing. Declared and initialized 2 score variables, created a loop to run the game 5 times

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice(){
     let computer_choice = Math.floor(Math.random() * 3) + 1;
     if(computer_choice === 1){
@@ -23,3 +26,38 @@ function getHumanChoice(){
         return "Invalid Number";
     }
 }
+
+function playRound(computerChoice, humanChoice){
+    if(computerChoice === "Rock" && humanChoice === "Scissors"){
+        console.log("you lose");
+        computerScore++;
+    } else if(computerChoice === "Paper" && humanChoice === "Rock"){
+        console.log("you lose");
+        computerScore++;
+    } else if(computerChoice === "Scissors" && humanChoice === "Paper"){
+        console.log("you lose");
+        computerScore++;
+    } else if(computerChoice === "Rock" && humanChoice === "Paper"){
+        console.log("you win");
+        humanScore++;
+    } else if(computerChoice === "Paper" && humanChoice === "Scissors"){
+        console.log("you win");
+        humanScore++;
+    } else if(computerChoice === "Scissors" && humanChoice === "Rock"){
+        console.log("you win");
+        humanScore++;
+    } else{
+        console.log("Draw");
+    }
+    console.log("Computer: " + computerChoice + " ; Human: " + humanChoice);
+    console.log("Scores: Computer = " + computerScore + " ; Human = " + humanScore);
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let compPlay = getComputerChoice();
+        let humanPlay = getHumanChoice();
+        playRound(compPlay, humanPlay);
+    };
+}
+playGame();
