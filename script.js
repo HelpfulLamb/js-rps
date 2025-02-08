f01012e5e14a67458f7f2b81f4f18bbbacb4b2de
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-02-07 20:33:55 -0600

    Added a new game button dom element after reaching 5 rounds

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
 let computerScore = 0;
 let humanScore = 0;
-let compPlay = '';
-let humanPlay = '';
+
+// round winning results (win, lose, draw)
+const result_container = document.querySelector(".results");
+
+const gameResult = document.createElement("h3");
+gameResult.classList.add("game-outcome");
+result_container.appendChild(gameResult);
+
+const movePlayed = document.createElement("p");
+movePlayed.classList.add("computer-move");
+result_container.appendChild(movePlayed);
+
+const userMove = document.createElement("p");
+userMove.classList.add("user-move");
+result_container.appendChild(userMove);
+
+const newGame = document.createElement("button");
+newGame.classList.add("new-game");
 
 
 function getComputerChoice(){
-    // 1 = rock, 2 = paper, 3 = scissors
-    let computer_choice = Math.floor(Math.random() * 3) + 1;
-    if(computer_choice === 1){
-        compPlay = 'Rock';
-    } else if(computer_choice === 2){
-        compPlay = 'Paper';
-    } else if(computer_choice === 3){
-        compPlay = 'Scissors';
-    }
-
-    if(computer_choice === 1 || computer_choice === 2 || computer_choice === 3){
-        return computer_choice;
-    } else {
-        return "Unknown";
-    }
+    const game_moves = ['rock', 'paper', 'scissors'];
+    let computer_choice = Math.floor(Math.random() * 3);
+    return game_moves[computer_choice];
 }
 
-function getHumanChoice(){
-    let player_choice = prompt("Choose: [1] Rock, [2] Paper, [3] Scissors").toLowerCase();
-    if(player_choice === "rock"){
-        humanPlay = 'Rock';
-        return 1;
-    } else if(player_choice === "paper"){
-        humanPlay = 'Paper';
-        return 2;
-    } else if(player_choice === "scissors"){
-        humanPlay = 'Scissors';
-        return 3;
-    } else{
-        return "Invalid Choice";
-    }
-}
 
 function playRound(computerChoice, humanChoice){
+    console.log("Computer: " + computerChoice + " ; Human: " + humanChoice);
     if(computerChoice === humanChoice){
-        console.log("Draw");
-    } else if((humanChoice === 1 && computerChoice === 3) || (humanChoice === 2 && computerChoice === 1) || (humanChoice === 3 && computerChoice === 2)){
-        console.log('You win');
+        gameResult.textContent = "Draw";
+        movePlayed.textContent = `Computer played ${computerChoice}`;
+        userMove.textContent = `You played ${humanChoice}`;
+    } else if((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')){
+        gameResult.textContent = "You Win!";
+        movePlayed.textContent = `Computer played ${computerChoice}`;
+        userMove.textContent = `You played ${humanChoice}`;
         humanScore++;
     } else{
-        console.log('You lose');
+        gameResult.textContent = "You Lose!";
+        movePlayed.textContent = `Computer played ${computerChoice}`;
+        userMove.textContent = `You played ${humanChoice}`;
         computerScore++;
     }
-    console.log("Computer: " + compPlay + " ; Human: " + humanPlay);
     console.log("Scores: Computer = " + computerScore + " ; Human = " + humanScore);
 }
 
-function playGame(){
-    for(let i = 0; i < 5; i++){
-        let compPlay = getComputerChoice();
-        let humanPlay = getHumanChoice();
-        playRound(compPlay, humanPlay);
-    };
-}
-playGame();
+
+const btn = document.querySelectorAll(".move");
+btn.forEach((button) => {
+    button.addEventListener("click", () => {
+        if(computerScore === 5 || humanScore === 5){
+            userMove.remove();
+            movePlayed.remove();
+            gameResult.textContent = "END";
+            newGame.textContent = "New Game";
+            result_container.appendChild(newGame);
+        } else {
+            let compPlay = getComputerChoice().toLowerCase();
+            playRound(compPlay, button.id.toLowerCase());
+        }
+    });
+});
